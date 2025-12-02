4385cf4d5a87d284003cd43da8e568549080443f
salvaharp-llc <salvaharp@gmail.com>
2025-12-02 17:55:14 +0100

    create queue as client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/salvaharp/peril/internal/gamelogic"
+	"github.com/salvaharp/peril/internal/pubsub"
+	"github.com/salvaharp/peril/internal/routing"
+)
 
 func main() {
+	const connString = "amqp://guest:guest@localhost:5672/"
+
 	fmt.Println("Starting Peril client...")
+
+	conn, err := amqp.Dial(connString)
+	if err != nil {
+		log.Fatalf("Unable to connect with RabbitMQ server: %v", err)
+	}
+	defer conn.Close()
+	fmt.Println("Successfully connected to RabbitMQ")
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalf("could not get username: %v", err)
+	}
+
+	_, queue, err := pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilDirect,
+		routing.PauseKey+"."+username,
+		routing.PauseKey,
+		pubsub.SimpleQueueTransient,
+	)
+	if err != nil {
+		log.Fatalf("could not subscribe to pause: %v", err)
+	}
+	fmt.Printf("Queue %v declared and bound!\n", queue.Name)
+
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Shutting down RabbitMQ connection")
 }
---
+++ internal/pubsub/consume.go
@@ -0,0 +1,40 @@
+package pubsub
+
+import (
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type SimpleQueueType int
+
+const (
+	SimpleQueueDurable SimpleQueueType = iota
+	SimpleQueueTransient
+)
+
+func DeclareAndBind(conn *amqp.Connection, exchange, queueName, key string, queueType SimpleQueueType,
+) (*amqp.Channel, amqp.Queue, error) {
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("could not create channel: %v", err)
+	}
+
+	queue, err := ch.QueueDeclare(
+		queueName,                       // name
+		queueType == SimpleQueueDurable, // durable
+		queueType != SimpleQueueDurable, // delete when unused
+		queueType != SimpleQueueDurable, // exclusive
+		false,                           // no-wait
+		nil,                             // args
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("could not declare queue: %v", err)
+	}
+
+	err = ch.QueueBind(queue.Name, key, exchange, false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("could not bind queue: %v", err)
+	}
+	return ch, queue, nil
+}
